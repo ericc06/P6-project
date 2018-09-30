c4d6c96f05f79e5ab74973c481d7b8b0907e5804
Eric Codron <eric.codron@gmail.com>
2018-10-01 00:56:29 +0200

    Trick details page dev started.

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -4,21 +4,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TrickController extends Controller
+class TrickController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function index()
     {
         $env = getenv('APP_ENV');
         $content = $this
             ->get('templating')
             ->render('trick/index.html.twig', array('nom' => $env));
+        return new Response($content);
+        //return new Response("test");
+    }
+
+    /**
+     * @Route("/trick/{trick_name}", name="trick_view", requirements={"trick_name"="\w+"})
+     */
+    public function view($trick_name)
+    {
+        $content = $this
+            ->get('templating')
+            ->render('trick/view.html.twig', array('nom' => $trick_name));
         return new Response($content);
         //return new Response("test");
     }
