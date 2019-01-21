a67a3f4eacb3b05d78bf630120feaae6b88a3eda
Eric Codron <eric.codron@gmail.com>
2019-01-21 18:47:32 +0100

    User registration en login form created (to be cleaned & improved) + i18n initialized.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,20 +5,18 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends Controller
 {
@@ -37,11 +35,11 @@ class UserController extends Controller
 
         // On ajoute les champs de l'entité que l'on veut à notre formulaire
         $formBuilder
-            //->add('fullname', TextType::class)
-            ->add('username', TextType::class)
+        //->add('fullname', TextType::class)
+        ->add('username', TextType::class)
             ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
-            //->add('avatar', FileType::class, array('label' => 'Votre avatar'))
+        //->add('avatar', FileType::class, array('label' => 'Votre avatar'))
             ->add('save', SubmitType::class)
         ;
 
@@ -68,13 +66,26 @@ class UserController extends Controller
                 $em->persist($user);
                 $em->flush();
 
+                $validation_url = "http://localhost/P6-project-flex/public/confirm?m=".$user->getEmail()."t=".$user->getActivationToken();
+
+                // Le message
+                $message = "Bonjour,\r\nVous avez demandé la création d'une compte utilisateur sur le site de référence du snowboard.\r\nPour confirmer votre inscription, veuillez cliquer sur ce lien.\r\nMerci.\r\nA très bientôt.";
+
+                // Dans le cas où nos lignes comportent plus de 70 caractères, nous les coupons en utilisant wordwrap()
+                $message = wordwrap($message, 70, "\r\n");
+
+                // Envoi du mail
+                //mail($user->getEmail, "Demande de confirmation d'inscription", $message);
+                mail("eric.codron@gmail.com", "Demande de confirmation d'inscription", $message);
+
+
                 $request->getSession()->getFlashBag()->add('notice', 'Un email de validation vous a été envoyé. Merci de le vérifier.');
 
                 // On redirige vers la page d'accueil
                 return $this->redirectToRoute('homepage', array());
             }
         }
-        
+
         // À ce stade, le formulaire n'est pas valide car :
         // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
         // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
@@ -99,11 +110,11 @@ class UserController extends Controller
 
         // On ajoute les champs de l'entité que l'on veut à notre formulaire
         $formBuilder
-            //->add('fullname', TextType::class)
-            ->add('username', TextType::class)
+        //->add('fullname', TextType::class)
+        ->add('username', TextType::class)
             ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
-            //->add('avatar', FileType::class, array('label' => 'Votre avatar'))
+        //->add('avatar', FileType::class, array('label' => 'Votre avatar'))
             ->add('save', SubmitType::class)
         ;
 
@@ -139,7 +150,6 @@ class UserController extends Controller
                     // ... handle exception if something happens during file upload
                     // TODO...
                 }
-
 
                 // updates the 'avatar' property to store the image file name
                 // instead of its contents
@@ -185,25 +195,25 @@ class UserController extends Controller
     /*
     public function registration(AuthenticationUtils $authenticationUtils): Response
     {
-        // Instancier objet User
-        // création de formulaire FormType
-        // handle request / isvalid
-        // Utilisation de l'encodeur de mdp de Symfony
-        // Entitymanager
+    // Instancier objet User
+    // création de formulaire FormType
+    // handle request / isvalid
+    // Utilisation de l'encodeur de mdp de Symfony
+    // Entitymanager
 
-        // erreur d'authentification (s'il y en a une)
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // dernier identifiant de connexion utilisé
-        $lastUsername = $authenticationUtils->getLastUsername();
+    // erreur d'authentification (s'il y en a une)
+    $error = $authenticationUtils->getLastAuthenticationError();
+    // dernier identifiant de connexion utilisé
+    $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('user/registration.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]
-        );
+    return $this->render('user/registration.html.twig', [
+    'last_username' => $lastUsername,
+    'error' => $error,
+    ]
+    );
     }
-    */
-    
+     */
+
     /**
      * @Route("/login", name="user_login")
      */
---
+++ src/Entity/Trick.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
+ */
+class Trick
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creation_date;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $last_update_date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creation_date;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creation_date): self
+    {
+        $this->creation_date = $creation_date;
+
+        return $this;
+    }
+
+    public function getLastUpdateDate(): ?\DateTimeInterface
+    {
+        return $this->last_update_date;
+    }
+
+    public function setLastUpdateDate(\DateTimeInterface $last_update_date): self
+    {
+        $this->last_update_date = $last_update_date;
+
+        return $this;
+    }
+}
