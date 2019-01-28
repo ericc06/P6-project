0c6e73c66e2ec2a1879d26f3a9d980d4ff93760d
Eric Codron <eric.codron@gmail.com>
2019-01-28 12:04:59 +0100

    UserForm used.

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,20 +6,33 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('fullName', TextType::class)
+            ->add('fullName', TextType::class, array('required' => false))
             ->add('username', TextType::class)
             ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
-            ->add('avatar', FileType::class)
+            ->add('avatar', FileType::class, array('required' => false))
             ->add('isActiveAccount', CheckboxType::class, array('required' => false))
-            ->add('activationToken', TextType::class)
-            ->add('roles')
+            ->add('activationToken', TextType::class, array('required' => false))
+            ->add('roles', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'entry_options' => [
+                    'attr' => ['class' => 'text-box'],
+                ],
+            ])
         ;
     }
 
