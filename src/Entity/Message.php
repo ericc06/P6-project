b3224d5dab657d806708438c34103348157d9c4f
Eric Codron <eric.codron@gmail.com>
2019-03-25 22:05:15 +0100

    Forum dev started.

---
+++ src/Entity/Message.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MessageRepository")
+ */
+class Message
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=2500, nullable=false)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     */
+    private $date;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Trick")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $trick = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getTrick(): ?Trick
+    {
+        return $this->trick;
+    }
+
+    public function setTrick(?Trick $trick): self
+    {
+        $this->trick = $trick;
+
+        return $this;
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -47,7 +48,7 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string")
      */
-    private $password ="";
+    private $password = "";
 
     /**
      * @ORM\Column(type="string", nullable=true)
@@ -55,6 +56,11 @@ class User implements UserInterface
      * @Assert\File(mimeTypes={ "image/jpeg", "image/png", "image/gif" })
      */
     private $avatar;
+
+    /**
+     * @ORM\Column(type="string", length=5, nullable=true)
+     */
+    private $fileExtension;
 
     /**
      * @ORM\Column(type="boolean")
@@ -65,7 +71,7 @@ class User implements UserInterface
      * @ORM\Column(type="string", nullable=true)
      */
     private $activationToken;
-    
+
     /**
      * @ORM\Column(type="json")
      */
@@ -141,9 +147,15 @@ class User implements UserInterface
         return $this->avatar;
     }
 
-    public function setAvatar(file $avatar): void
+    public function setAvatar(UploadedFile $avatar)
     {
         $this->avatar = $avatar;
+
+        // On vérifie si on avait déjà un fichier pour cette entité
+        if (null !== $this->fileUrl) {
+            // On sauvegarde l'extension du fichier pour le supprimer plus tard
+            $this->tempFilename = $this->fileUrl;
+        }
     }
 
     public function getIsActiveAccount()
@@ -217,4 +229,102 @@ class User implements UserInterface
 
         return $this;
     }
+
+    public function getFileExtension(): ?string
+    {
+        return $this->fileExtension;
+    }
+
+    public function setFileExtension(string $fileExtension): self
+    {
+        $this->fileExtension = $fileExtension;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        // Si jamais il n'y a pas de fichier (champ facultatif), on ne fait rien
+        if (null === $this->avatar) {
+            return;
+        }
+
+        // Le nom du fichier est son id, on doit juste stocker également son extension
+        // Pour faire propre, on devrait renommer cet attribut en « extension », plutôt que « url »
+        $this->fileExtension = $this->avatar->guessExtension();
+
+        // Et on génère l'attribut alt de la balise <img>, à la valeur du nom du fichier sur le PC de l'internaute s'il n'est pas renseigné dans le formulaire
+        /*if (null === $this->alt) {
+            $this->alt = $this->avatar->getClientOriginalName();
+        }
+        */
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        // Si jamais il n'y a pas de fichier (champ facultatif), on ne fait rien
+        if (null === $this->avatar) {
+            return;
+        }
+
+        // Si on avait un ancien fichier, on le supprime
+        if (null !== $this->tempFilename) {
+            $oldFile = $this->getUploadRootDir() . '/' . $this->id . '.' . $this->tempFilename;
+            if (file_exists($oldFile)) {
+                unlink($oldFile);
+            }
+        }
+
+        // On déplace le fichier envoyé dans le répertoire de notre choix
+        $this->avatar->move(
+            $this->getUploadRootDir(), // Le répertoire de destination
+            $this->id . '.' . $this->fileExtension// Le nom du fichier à créer, ici « id.extension »
+        );
+    }
+
+    /**
+     * @ORM\PreRemove()
+     */
+    public function preRemoveUpload()
+    {
+        // On sauvegarde temporairement le nom du fichier, car il dépend de l'id
+        $this->tempFilename = $this->getUploadRootDir() . '/' . $this->id . '.' . $this->fileExtension;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        // En PostRemove, on n'a pas accès à l'id, on utilise notre nom sauvegardé
+        if (file_exists($this->tempFilename)) {
+            // On supprime le fichier
+            unlink($this->tempFilename);
+        }
+    }
+
+    public function getUploadDir()
+    {
+        // On retourne le chemin relatif vers l'image pour un navigateur
+        return 'uploads/images/users';
+    }
+
+    protected function getUploadRootDir()
+    {
+        // On retourne le chemin relatif vers l'image pour notre code PHP
+        return __DIR__ . '/../../public/' . $this->getUploadDir();
+    }
+
+    public function getFixturesPath()
+    {
+        return __DIR__ . '/../../src/DataFixtures/images/users';
+    }    
 }
