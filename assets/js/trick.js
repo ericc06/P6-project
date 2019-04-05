70193c2d6a5d54f04ed932eb18d63e6d8a4f9ab6
Eric Codron <eric.codron@gmail.com>
2019-04-06 00:06:40 +0200

    Trick edition improved. WIP.

--- assets/js/trick.js
+++ assets/js/trick.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
     // For videos we hide the file upload field.
     $("input[id$='_fileType'][value='0']").parentsUntil("fieldset.form-group").find("input[id$='_fileUrl']").parent('div').css('visibility', 'hidden').css('height', '0').css('margin', '0');
 
-    $("input[id$='_fileType'][value='1']").parentsUntil("fieldset.form-group").find("input[id$='_file']").parent('div').css('visibility', 'hidden').css('height', '0').css('margin', '0');//.detach();
+    $("input[id$='_fileType'][value='1']").parentsUntil("fieldset.form-group").find("input[id$='_file']").parent('div').css('visibility', 'hidden').css('height', '0').css('margin', '0');
     
-    $("input[id$='_fileType'][value='1']").parentsUntil("fieldset.form-group").find("input[id$='_defaultCover']").parent('div').css('visibility', 'hidden').css('height', '0').css('margin', '0').parent('div').css('margin-bottom', '0');//.detach();
+    $("input[id$='_fileType'][value='1']").parentsUntil("fieldset.form-group").find("input[id$='_defaultCover']").parent('div').css('visibility', 'hidden').css('height', '0').css('margin', '0').parent('div').css('margin-bottom', '0');
 
     // On récupère la balise <div> en question qui contient l'attribut "data-prototype" qui nous intéresse.
     var $container = $('div#trick_medias');
@@ -30,7 +30,6 @@ $(document).ready(function () {
         }
         // On ajoute le prototype modifié à la fin de la balise <div>
         $container.append($prototype.css('display', 'block').css('border', 'none'));
-        //$('div.add_media_buttons_div').hide();
         
         switch ($mediaType) {
             case 'image':
@@ -47,6 +46,12 @@ $(document).ready(function () {
                 // On initialise le champ hidden "fileType" à 0 (valeur pour une image)
                 $('input#trick_medias_' + index + '_fileType').val(0);
                 //$('input#trick_medias_' + index + '_defaultCover').attr('name', 'defaultCover');//.attr('checked', false);
+
+                // We set the "required" attribute to all required elements 
+                $('input#trick_medias_' + index + '_file').attr("required", "required");
+                $('input#trick_medias_' + index + '_title').attr("required", "required");
+                $('input#trick_medias_' + index + '_alt').attr("required", "required");
+
                 break;
             case 'video':
                 // On cache le champ d'upload de l'image et on montre celui de saisie de l'URL de la video
@@ -55,6 +60,12 @@ $(document).ready(function () {
                 $('input#trick_medias_' + index + '_fileUrl').parent().closest('div').show();
                 // On initialise le champ hidden "fileType" à 1 (valeur pour une video)
                 $('input#trick_medias_' + index + '_fileType').val(1);
+
+                // We set the "required" attribute to all required elements 
+                $('input#trick_medias_' + index + '_fileUrl').attr("required", "required");
+                $('input#trick_medias_' + index + '_title').attr("required", "required");
+                $('input#trick_medias_' + index + '_alt').attr("required", "required");
+
                 break;
             default:
                 break;
@@ -89,14 +100,25 @@ $(document).ready(function () {
         return false;
     });
 
-    // On ajoute un premier champ automatiquement s'il n'en existe pas déjà un (cas d'une nouvelle figure par exemple).
+    // On ajoute un premier champ automatiquement s'il n'en existe pas déjà un
+    // (cas d'une nouvelle figure par exemple).
     if (index === 0) {
         addMedia($container, 'image', translations['at_least_one_image_trans'], false);
-    } else { // S'il existe déjà des medias, on ajoute un lien de suppression pour chacun d'entre eux
+    } else {
+        // S'il existe déjà des medias, on ajoute un lien de suppression
+        // pour chacun d'entre eux. C'est le cas si le formulaire est rechargé
+        // après une erreur de validation.
+        // Par contre il ne faut pas ces boutons pour les médias dejà existants
+        // en base (cas de l'édition d'un trick).
         $container.children('fieldset').each(function () {
             let fieldsetNumber = $(this).children('legend').html();
             if (fieldsetNumber !== '0') {
-                addDeleteLink($(this));
+                // Existing medias (trick modification) have a "#trick_medias_X_id"
+                // hidden input with a numeric value (media id).
+                // This value is not present for non existing medias.
+                if(! $('#trick_medias_' + fieldsetNumber + '_id').val()) {
+                    addDeleteLink($(this));
+                }
                 if ($(this).children('div#trick_medias_' + fieldsetNumber).children('input#trick_medias_'+fieldsetNumber+'_fileType').val() === 0) {
                     $(this).children('legend').html(translations['added_image_trans']);
                 } else {
@@ -105,11 +127,6 @@ $(document).ready(function () {
             } else {
                 $(this).children('legend').html(translations['at_least_one_image_trans']);
             }
-            // For all images we give the radio button the same name so that
-            // they all work together to uncheck all others when one is checked.
-            /*if ($(this).children('div#trick_medias_' + fieldsetNumber).children('input#trick_medias_'+fieldsetNumber+'_fileType').val() === 0) {
-                $(this).find('input#trick_medias_' + fieldsetNumber + '_defaultCover').attr('name', 'defaultCover');
-            }*/
         });
     }
 
@@ -126,15 +143,13 @@ $(document).ready(function () {
     // checking one unchecks all others.
     $('form input:radio').on('click', function (e) {
         let radioName = $(this).attr('original-name')
-        //$(this).attr('name', $radioName);
         $('form input:radio').attr('name', radioName);
     });
 
     // When the form submit button is clicked, we disable it
     // to avoid double submission.
-    $('input[type=submit]').on('click', function (e) {
-        e.preventDefault();
-        $(this).attr('disabled', true);
-        $('.trick_form').submit();
+    $("form").submit(function () {
+        $("input[type=submit]").attr("disabled", true);
+        return true;
     });
 });
